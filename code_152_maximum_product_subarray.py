0ad072b4aca317d2237b09d2102a468bc13645ed
mjfox33 <mjfox33@gmail.com>
2022-02-07 20:19:46 -0500

    initial commit

---
+++ code_152_maximum_product_subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        n=len(nums)
+        if n < 2:
+            return 0
+        max_product = 0
+        for i in range(n-1):
+            product = nums[i] * nums[i+1]
+            if product > max_product:
+                max_product = product
+        return max_product
