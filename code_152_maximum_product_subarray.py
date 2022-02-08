021ac8c19479347e705ee9139992a49980da5cec
mjfox33 <mjfox33@gmail.com>
2022-02-07 20:20:47 -0500

    added a note

--- code_152_maximum_product_subarray.py
+++ code_152_maximum_product_subarray.py
@@ -9,3 +9,6 @@ class Solution:
             if product > max_product:
                 max_product = product
         return max_product
+
+# this needs work as i misunderstood the question
+# the subarray can be len(nums) or 1 digit
